13a6b2cbc4549ad5d4f4f95a8f34beced9b4c44a
IshaNITM <ishap6971@gmail.com>
2025-04-13 17:57:30 +0530

    Enabled some test case for basic testing of Employee Management App

--- employee-management/src/app/app.component.spec.ts
+++ employee-management/src/app/app.component.spec.ts
@@ -14,16 +14,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'employee-management' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('employee-management');
-  });
-
-  it('should render title', () => {
+  it('should contain the employee list component', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, employee-management');
+    expect(compiled.querySelector('app-employee-list')).toBeTruthy();
   });
 });
